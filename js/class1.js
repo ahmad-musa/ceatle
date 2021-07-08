e1bb5a85c34b115b81f6deb10b352aac4b49cbc6
ahmad-musa <ahmadmusa0987@gmail.com>
2021-07-09 00:13:14 +0600

    project init

---
+++ js/class1.js
@@ -0,0 +1,21 @@
+console.log("Salamun Alaika");
+console.log('54521asd');
+console.log(`my roll: ${21542343}`);
+
+var a;
+let b;
+const c=3;
+
+a=10;
+b=20;
+
+console.log(a,b);
+console.log(a+b);
+console.log("a+b =",a+b);
+console.log(
+    "a = " +a,
+    "b = " +b,
+    "c = " +c,
+    "a+b = " + a + b,
+    "a+b = " + (a+b)
+);
