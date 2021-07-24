a02a9972413ab6e8d913bf0819626cc6b494256c
ahmad-musa <ahmadmusa0987@gmail.com>
2021-07-24 20:29:50 +0600

    kichu na

--- js/class1.js
+++ js/class1.js
@@ -5,9 +5,13 @@ console.log(`my roll: ${21542343}`);
 var a;
 let b;
 const c=3;
+let d=20.52;
+let e=21.21;
+let f='S';
+let g="Shakib";
 
-a=10;
-b=20;
+a=20;
+b=10;
 
 console.log(a,b);
 console.log(a+b);
@@ -19,3 +23,32 @@ console.log(
     "a+b = " + a + b,
     "a+b = " + (a+b)
 );
+
+console.log('Sum = a+b = ', a+b);
+console.log('Substract = a-b = ', a-b);
+console.log('Multiplication = a*b = ', a*b);
+console.log('Division = a/b = ', a/b);
+
+console.log(e, typeof e);
+console.log(f, typeof f);
+console.log(g, typeof g);
+
+h=null;
+console.log(h, typeof h);
+
+i=true;
+console.log(i, typeof i);
+
+j=undefined;
+console.log(j, typeof j);
+
+let student = {
+    name: "Abdul",
+    roll: "45",
+    class:"10"
+};
+console.log(student, typeof student);
+console.log(student.name, typeof student.name);
+
+let arr=[11,13,15,"Saffy", "puffy"]
+console.log(arr, typeof arr);
---
+++ js/learn.js
@@ -0,0 +1,30 @@
+function out(sms){
+    console.log(sms);
+}
+
+out("Hello");
+out("Jello");
+out("Gello");
+
+function out(sms, detail){
+    console.log(sms);
+    console.log(detail);
+}
+
+out("Good Morning", "Wish for a sound and healthy day");
+
+let name, roll, year;
+name= "Jhonny";
+roll= 18;
+year="Nine";
+
+function gets(name,roll,year) {
+    console.log(`Your name is ${name}`);
+    console.log("Roll is " +roll);
+    console.log(`Class is ${year}`);
+    
+}
+
+gets(name,roll,year);
+
+gets('AR Khan',25,'Ten');
